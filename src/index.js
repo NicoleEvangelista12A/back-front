78e45eab7fc1cc95026a1b6df452ba1458ac1dc9
NicoleEvangelista12A <nicole.barross@eaportal.org>
2025-11-05 21:53:56 -0300

    tudo pronto

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import express from "express"
 import { persons } from "./persons.js";
 import cors from "cors"
 const app = express()
-const port = 3333
+const port = 3000
 app.use(cors())
+app.use( express.json())
 
 // GET, POST, DELETE, PATCH, PUT
 
 app.get("/", (request, response) => {
   response.json( persons )
+})
+
+app.post("/cadastrar", (request, response) => {
+  const {name, email, age, nickname, passaword} =request.body.user
+  console.log(`${name}, ${email}, ${age}, ${nickname}, ${passaword}`)
+
+  response.status(201).json({ massage: "usuario cadastrado com sucesso" })
 })
 
 app.listen(port, () => {
