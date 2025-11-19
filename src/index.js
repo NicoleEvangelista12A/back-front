d4dc23585b01c08a85e7f9a31114af566bcde323
NicoleEvangelista12A <nicole.barross@eaportal.org>
2025-11-19 09:30:57 -0300

    cabei

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from "express"
-import { persons } from "./persons.js";
 import cors from "cors"
+import mysql from "mysql2"
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env
+
 const app = express()
 const port = 3000
 app.use(cors())
@@ -9,16 +12,41 @@ app.use( express.json())
 // GET, POST, DELETE, PATCH, PUT
 
 app.get("/", (request, response) => {
-  response.json( persons )
-})
+  const selectCommand = `
+    SELECT name, email, age, nickname
+    FROM marciomarcal_02ta
+  `;
+});
+
 
 app.post("/cadastrar", (request, response) => {
   const {name, email, age, nickname, passaword} =request.body.user
-  console.log(`${name}, ${email}, ${age}, ${nickname}, ${passaword}`)
+  
+  const insertCommand = `
+  INSERT INTO nicolebarros_02ta(name, email, age, nickname, passaword)
+  VALUES(?, ?, ?, ?, ?)
+  `
+  database.query(insertCommand [name, email, age, nickname, passaword], (error) => {
+    if (error) {
+    console.log(error)
+    return
+}
 
-  response.status(201).json({ massage: "usuario cadastrado com sucesso" })
+ response.json(users)
+
+  })
+
+ 
 })
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}!`)
+})
+
+const database = mysql.createPool({
+  host: "DB_HOST",
+  user: "DB_USER",
+  password: "DB_PASSWORD",
+  database: "DB_NAME",
+  connectionLimit: 10
 })
